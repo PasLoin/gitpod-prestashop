7cb2c72106830ee109fd657d5c560d96b23f2f11
PasLoin <raphael0arnould@gmail.com>
2021-01-16 05:27:50 +0100

    Create .bashrc.sh

---
+++ conf/.bashrc.sh
@@ -0,0 +1,9 @@
+#### Prestashop  Setup Script ####
+### WIP ###
+# export REPO_NAME=$(basename $GITPOD_REPO_ROOT)
+#  move the workspace temporarily
+#  mkdir $HOME/workspace
+#  mv ${GITPOD_REPO_ROOT}/* $HOME/workspace/ 
+# create a database for Prestashop # 
+# install PrestaShop
+  
